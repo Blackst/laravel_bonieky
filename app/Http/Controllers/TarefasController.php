7fe051af083686a8c20ab366d7184bffcc3d0835
Thiago Mendes <thiagomendeswd@gmail.com>
2020-02-12 11:53:36 -0300

    Inserido eloquent no TarefasCOntroller

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 
+use App\Tarefa;
+
 class TarefasController extends Controller
 {
      public function list(){
-        $list = DB::select('SELECT * FROM tarefas');
+        $list = Tarefa::all();
 
         return view('tarefas.list', ['list' => $list]);
      }
@@ -25,17 +27,19 @@ class TarefasController extends Controller
 
           $titulo = $request->input('titulo');
 
-          DB::insert('INSERT INTO tarefas (titulo) VALUES (:titulo)', ['titulo'=>$titulo]);
+          $t = new Tarefa;
+          $t->titulo = $titulo;
+          $t->save();
 
           return redirect()->route('tarefas.list');
     }
 
     public function edit($id){
 
-          $data = DB::select('SELECT * FROM tarefas WHERE id = :id', ['id' => $id]);
+          $data = Tarefa::find($id);
 
-          if (count($data) > 0) {
-               return view('tarefas.edit', ['data' => $data[0]]); //Pega primeiro item da consulta.
+          if ($data) {
+               return view('tarefas.edit', ['data' => $data]);
           }else{
                return redirect()->route('tarefas.list');
           }
@@ -49,20 +53,28 @@ class TarefasController extends Controller
           ]); //É requerido e o campo é uma string.
          
           $titulo = $request->input('titulo');
-                   
-          DB::update('UPDATE tarefas SET titulo = :titulo WHERE id = :id', ['id' => $id, 'titulo' => $titulo] );
+
+          $t = Tarefa::find($id);
+          $t->titulo = $titulo;
+          $t->save();
           
           return redirect()->route('tarefas.list');
     }
 
     public function del($id){
-         DB::delete('DELETE FROM tarefas WHERE id = :id', ['id'=> $id]);
+
+         Tarefa::find($id)->delete();
 
          return redirect()->route('tarefas.list');
     }
 
     public function done($id){
-         DB::update('UPDATE tarefas SET resolvido = 1 - resolvido WHERE id = :id', ['id' => $id]);
+
+         $t = Tarefa::find($id);
+         if ($t) {
+          $t->resolvido = 1 - $t->resolvido;
+          $t->save();
+         }
 
          return redirect()->route('tarefas.list');
     }
